0b2c793f99cb210d08d386b24167a163ba16042e
Piotrek <piotrziobrowski.work@gmail.com>
2024-06-15 00:27:25 +0200

    WC_2014

--- backend/script.py
+++ backend/script.py
@@ -161,6 +161,39 @@ def clean_player_name(player_name):
         "Sun Jihai": "Jihai Sun",
         "Ma Mingyu": "Mingyu Ma",
         "Hao Haidong": "Haidong Hao",
+        "Li Xiaopeng": "Xiaopeng Li",
+        "Qi Hong": "Hong Qi",
+        "Xu Yunlong": "Yunlong Xu",
+        "Jiang Jin": "Jin Jiang",
+        "Ou Chuliang": "Chuliang Ou",
+        "Lee Woon-jae": "Woon-Jae Lee",
+        "Hyun Young-min": "Young-Min Hyun",
+        "Choi Sung-yong": "Sung-Yong Choi",
+        "Choi Jin-cheul": "Jin-Cheul Choi",
+        "Kim Nam-il": "Nam-Il Kim",
+        "Yoo Sang-chul": "Sang-Chul Yoo",
+        "Kim Tae-young": "Tae-Young Kim",
+        "Choi Tae-uk": "Tae-Uk Choi",
+        "Seol Ki-hyeon": "Ki-Hyeon Seol",
+        "Lee Young-pyo": "Young-Pyo Lee",
+        "Choi Yong-soo": "Yong-Soo Choi",
+        "Kim Byung-ji": "Byung-Ji Kim",
+        "Lee Eul-yong": "Eul-Yong Lee",
+        "Lee Chun-soo": "Chun-Soo Lee",
+        "Lee Min-sung": "Min-Sung Lee",
+        "Cha Du-ri": "Du-Ri Cha",
+        "Yoon Jong-hwan": "Jong-Hwan Yoon",
+        "Hwang Sun-hong": "Sun-Hong Hwang",
+        "Ahn Jung-hwan": "Jung-Hwan Ahn",
+        "Hong Myung-bo": "Myung-Bo Hong",
+        "Park Ji-sung": "Ji-Sung Park",
+        "Song Chong-gug": "Chong-Gug Song",
+        "Choi Eun-sung": "Eun-Sung Choi",
+        "Patrick M'Boma": "Patrick Mboma",
+        "Daniel N'Gom Kome": "Daniel Komé",
+        "Valeri Karpin": "Valeriy Karpin",
+        "Yegor Titov": "Egor Titov",
+        "Dmitri Khokhlov": "Dmitriy Khokhlov",
         "Yu Genwei": "Genwei Yu",
         "Su Maozhen": "Maozhen Su",
         "Gao Yao": "Yao Gao",
@@ -168,11 +201,143 @@ def clean_player_name(player_name):
         "Zhao Junzhe": "Junzhe Zhao",
         "Qu Bo": "Bo Qu",
         "Du Wei": "Wei Du",
-        "Li Xiaopeng": "Xiaopeng Li",
-        "Qi Hong": "Hong Qi",
-        "Xu Yunlong": "Yunlong Xu",
-        "Jiang Jin": "Jin Jiang",
-        "Ou Chuliang": "Chuliang Ou",
+        "Eric Djemba-Djemba": "Eric Djemba Djemba",
+        "Daniel Ngom Kome": "Daniel Kome",
+        "Mohamed Al-Deayea": "Mohammad Al-Deayea",
+        "Abdullah Zubromawi": "Abdullah Sulaiman Zubromawi",
+        "Ahmed Al-Dokhi": "Ahmad Al-Dokhi",
+        "Abdullah Al-Jumaan": "Abdullah Jumaan Al-Dossary",
+        "Mohammed Al-Khojali": "Mohammad Khojahali",
+        "Mansour Al-Thagafi": "Mansoor Al-Thagafi",
+        "Augusto Porozo": "Augusto Poroso",
+        "Franky Vandendriessche": "Franky Van Der Elst",
+        "Yuri Kovtun": "Yuriy Kovtun",
+        "Yuri Nikiforov": "Yuriy Nikiforov",
+        "Vyacheslav Dayev": "Vyacheslav Daev",
+        "Igor Chugainov": "Igor Chugaynov",
+        "Zoubeir Baya": "Zoubaier Baya",
+        "Ziad Jaziri": "Zied Jaziri",
+        "Imed Mhedhebi": "Imed Mhadhebi",
+        "Emir Mkademi": "Amir Mkadmi",
+        "Ahmed El-Jaouachi": "Ahmed Jaouachi",
+        "Gilles Yapi Yapo": "Gilles Yapi",
+        "Predrag Đorđević": "Predrag Djordjevic",
+        "Nenad Đorđević": "Nenad Djordjevic",
+        "Dušan Petković[29]": "Dusan Petkovic",
+        "Paulo Figueiredo": "Figueiredo",
+        "Edson Nobre": "Edson",
+        "Amir Hossein Sadeghi": "Amirhossein Sadeghi",
+        "Haminu Draman": "Draman Haminu",
+        "Alessandro Santos": "Alex",
+        "Kim Young-chul": "Young-chul Kim",
+        "Kim Jin-kyu": "Jin-kyu Kim",
+        "Kim Do-heon": "Do-heon Kim",
+        "Park Chu-young": "Chu-young Park",
+        "Baek Ji-hoon": "Ji-hoon Baek",
+        "Chung Kyung-ho": "Kyung-ho Chung",
+        "Kim Sang-sik": "Sang-sik Kim",
+        "Cho Jae-jin": "Jae-jin Cho",
+        "Kim Yong-dae": "Yong-dae Kim",
+        "Kim Young-kwang": "Young-kwang Kim",
+        "Cho Won-hee": "Won-hee Cho",
+        "Chérif Touré Mamam": "Cheriffe Maman-Touré",
+        "Hamad Al-Montashari": "Hamad Al Montashari",
+        "Mohammed Ameen": "Mohammad Ameen",
+        "Mohammad Massad": "Mohammed Massad",
+        "Mohammad Khouja": "Mohammad Khoja",
+        "Haykel Guemamdia": "Haykel Gmamdia",
+        "Francileudo Santos": "Santos",
+        "Jawhar Mnari": "Jaouhar Mnari",
+        "Sofiane Melliti": "Sofiene Melliti",
+        "Oleh Shelayev": "Oleg Shelayev",
+        "Nikos Spyropoulos": "Nikolaos Spyropoulos",
+        "Rúben Amorim": "Ruben Amorim",
+        "Sakis Prittas": "Athanasios Prittas",
+        "Oh Beom-seok": "Beom-seok Oh",
+        "Kim Hyung-il": "Hyung-il Kim",
+        "Cho Yong-hyung": "Yong-hyung Cho",
+        "Kim Bo-kyung": "Bo-kyung Kim",
+        "Kim Jung-woo": "Jung-woo Kim",
+        "Lee Seung-yeoul": "Seung-yeoul Lee",
+        "Kim Jae-sung": "Jae-sung Kim",
+        "Lee Jung-soo": "Jeong-soo Lee",
+        "Ki Sung-yueng": "Sung-yueng Ki",
+        "Lee Chung-yong": "Chung-yong Lee",
+        "Jung Sung-ryong": "Sung-ryong Jung",
+        "Yeom Ki-hun": "Ki-hun Yeom",
+        "Lee Dong-gook": "Dong-gook Lee",
+        "Kang Min-soo": "Min-soo Kang",
+        "Antar Yahia": "Anthar Yahia",
+        "Faouzi Chaouchi": "Fawzi Chaouchi",
+        "Michael Dawson[5]": "Michael Dawson",
+        "Eugene Galekovic[6]": "Eugene Galekovic",
+        "Dragan Mrđa": "Dragan Mrdja",
+        "Anđelko Đuričić": "Andjelko Djuricic",
+        "Eric Maxim Choupo-Moting": "Eric-Maxim Choupo-Moting",
+        "Mohammadou Idrissou": "Mohamadou Idrissou",
+        "Guy N'dy Assembé": "Guy Ndy Assembé",
+        "Ben Sigmund": "Benjamin Sigmund",
+        "Andy Barron": "Andrew Barron",
+        "Dave Mulligan": "David Mulligan",
+        "Cheick Tioté": "Cheik Tioté",
+        "Ri Myong-guk": "Myong-guk Ri",
+        "Cha Jong-hyok": "Jong-hyok Cha",
+        "Ri Jun-il": "Jun-il Ri",
+        "Ri Kwang-chon": "Kwang-chon Ri",
+        "Kim Kum-il": "Kum-il Kim",
+        "An Chol-hyok": "Chol-hyok An",
+        "Ji Yun-nam": "Yun-nam Ji",
+        "Jong Tae-se": "Chong Tese",
+        "Hong Yong-jo": "Yong-jo Hong",
+        "Mun In-guk": "In-guk Mun",
+        "Choe Kum-chol": "Kum-Chol Choe",
+        "Pak Chol-jin": "Chol-jin Pak",
+        "Kim Yong-jun": "Yong-jun Kim",
+        "Nam Song-chol": "Song-chol Nam",
+        "An Yong-hak": "Yeong-hak Ahn",
+        "Kim Myong-gil": "Myong-gil Kim",
+        "Ri Chol-myong": "Chol-myong Ri",
+        "Kim Myong-won[9]": "Myong-won Kim",
+        "Ri Kwang-hyok": "Kwang-hyok Ri",
+        "Kim Kyong-il": "Kyong-il Kim",
+        "Pak Sung-hyok": "Sung-Hyok Pak",
+        "Rúben Amorim[10]": "Rúben Amorim",
+        "Jerry Palacios[11]": "Jerry Palacios",
+        "Éder Álvarez Balanta": "Éder Balanta",
+        "Giorgos Tzavellas": "Georgios Tzavellas",
+        "Sayouba Mandé": "Mandé Sayouba",
+        "David Myrie[64]": "David Myrie",
+        "Rémy Cabella[90]": "Rémy Cabella",
+        "Stéphane Ruffier[92]": "Stéphane Ruffier",
+        "Morgan Schneiderlin[94]": "Morgan Schneiderlin",
+        "Edder Delgado[97]": "Edder Delgado",
+        # "Ghasem Haddadifar": "Brown Ideye",
+        "Kunle Odunlami": "Odunlami Kunle",
+        "Emmanuel Agyemang-Badu": "Emmanuel Badu",
+        "Adam Larsen Kwarasey": "Adam Kwarasey",
+        "Fatau Dauda": "Fatawu Dauda",
+        "Cédric Si Mohamed": "Cédric Si Mohammed",
+        "Aleksei Kozlov": "Aleksey Kozlov",
+        "Viktor Fayzulin": "Viktor Faizulin",
+        "Andrey Yeshchenko": "Andrey Eshchenko",
+        "Brown Ideye[4]": "Brown Ideye",
+        "Kim Chang-soo": "Chang-soo Kim",
+        "Yun Suk-young": "Suk-young Yun",
+        "Kwak Tae-hwi": "Tae-hwi Kwak",
+        "Kim Young-gwon": "Young-gwon Kim",
+        "Hwang Seok-ho": "Seok-ho Hwang",
+        "Ha Dae-sung": "Dae-sung Ha",
+        "Son Heung-min": "Heung-min Son",
+        "Lee Keun-ho": "Keun-ho Lee",
+        "Koo Ja-cheol": "Ja-cheol Koo",
+        "Han Kook-young": "Kook-young Han",
+        "Park Jong-woo": "Jong-woo Park",
+        "Kim Shin-wook": "Shin-wook Kim",
+        "Ji Dong-won": "Dong-won Ji",
+        "Hong Jeong-ho": "Jeong-ho Hong",
+        "Kim Seung-gyu": "Seung-gyu Kim",
+        "Park Joo-ho[159]": "Joo-ho Park",
+        "Lee Bum-young": "Bum-young Lee"
     }
     return replacements.get(player_name, player_name)
 
@@ -191,6 +356,10 @@ def get_transfermarkt_id(player_name):
         profile_url = player_link['href']
         player_id = re.search(r'/profil/spieler/(\d+)', profile_url).group(1)
         return int(player_id)
+    elif player_name == "Pak Nam-chol I":
+        return 68565
+    elif player_name == "Pak Nam-chol II":
+        return 114610
     else:
         print(f"No player profile link found for {player_name}")
         return None
@@ -281,10 +450,10 @@ def save_market_value_history(player_id, conn):
 # # Pobranie danych drużyn
 # scrap_squad("https://en.wikipedia.org/wiki/UEFA_Euro_2024_squads", conn)
 
-tournament = "world_cup"
-conn = create_database(tournament, 2002)
-# Pobranie danych drużyn
-scrap_squad("https://en.wikipedia.org/wiki/2002_FIFA_World_Cup_squads", conn)
+# tournament = "world_cup"
+# conn = create_database(tournament, 2002)
+# # Pobranie danych drużyn
+# scrap_squad("https://en.wikipedia.org/wiki/2002_FIFA_World_Cup_squads", conn)
 # tournament = "world_cup"
 # conn = create_database(tournament, 2006)
 # # Pobranie danych drużyn
@@ -293,10 +462,10 @@ scrap_squad("https://en.wikipedia.org/wiki/2002_FIFA_World_Cup_squads", conn)
 # conn = create_database(tournament, 2010)
 # # Pobranie danych drużyn
 # scrap_squad("https://en.wikipedia.org/wiki/2010_FIFA_World_Cup_squads", conn)
-# tournament = "world_cup"
-# conn = create_database(tournament, 2014)
-# # Pobranie danych drużyn
-# scrap_squad("https://en.wikipedia.org/wiki/2014_FIFA_World_Cup_squads", conn)
+tournament = "world_cup"
+conn = create_database(tournament, 2014)
+# Pobranie danych drużyn
+scrap_squad("https://en.wikipedia.org/wiki/2014_FIFA_World_Cup_squads", conn)
 # tournament = "world_cup"
 # conn = create_database(tournament, 2018)
 # # Pobranie danych drużyn
